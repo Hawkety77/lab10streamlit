d8dc806348dc3953bb44a3645f3c15eee352c043
Ty Hawkes <hawkety77@gmail.com>
2023-11-26 16:33:12 -0700

    first commit

---
+++ lab-10.py
@@ -0,0 +1,9 @@
+# Create a Streamlit app of your choice and deploy it using GitHub and Streamlit Community Cloud.  
+# Your app should have at least two interactive elements and at least 
+# one graph that updates based on the input. Submit the URL of your public Streamlit App. 
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('Popular Names')
