b1deec15f7a92af96c9a3f63fef21e40a4015b9a
Ty Hawkes <hawkety77@gmail.com>
2023-11-26 17:21:53 -0700

    second commit

--- lab-10.py
+++ lab-10.py
@@ -6,4 +6,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title('Popular Names')
+df = pd.read_csv('popular_names.csv')
+
+st.title('Top 20 Most Popular Baby Names by Year')
+
+user_input = st.slider('Select a Year:', 1910, 2021, 2021)
+selected_option = st.selectbox('Select a Gender:', ['Male', 'Female'])
+if selected_option == 'Male':
+    sex = 'M'
+else:
+    sex = 'F'
+data = df[(df['year'] == user_input) & (df['sex'] == sex)].sort_values('n', ascending = False).head(20)
+
+fig = px.bar(data, x = 'name', y = 'n')
+
+st.plotly_chart(fig)
